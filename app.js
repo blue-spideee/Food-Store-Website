cd42a1ac1bee684e95222a8d05f8733422404c4b
KM Fahim Mahmud <km.fahiimmahmud@gmail.com>
2022-03-10 19:51:50 +0600

    Connect

---
+++ app.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var x=document.getElementById("header__nav-bar__menu");
+    if (x.className==="hidden"){
+        x.className="show";
+    }else{
+        x.className="hidden";
+    }
+}
